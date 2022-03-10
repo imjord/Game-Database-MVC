5514a00897dd052c92cced62717261ae007634f7
imjord <dean.jay.goat@gmail.com>
2022-03-10 15:06:48 -0700

    trying to mess with icon

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 // imports
-const express = require('express');
-const db = require('./config/connection.js');
+const express = require("express");
+const db = require("./config/connection.js");
 const app = express();
-const path = require('path');
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 // middleware
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // views
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-// routes 
-const UserRoutes = require('./routes/User');
-const createGame = require('./routes/Game');
+// routes
+const UserRoutes = require("./routes/User");
+const createGame = require("./routes/Game");
 
-app.use('/users', UserRoutes);
-app.use('/games', UserRoutes);
-app.use('/addgames', createGame);
-  
- 
-app.get('/', (req,res) =>{
-    res.render('index', {title: "Home"})
-}) 
-//404 
-app.use("*", (req,res) => {
-    res.status(404).render('404', {title: "404"})
-})
-// start server with db 
+app.use("/users", UserRoutes);
+app.use("/games", UserRoutes);
+app.use("/addgames", createGame);
 
-db.once('open', ()=> {
-    app.listen({port: 3000}, () =>{
-        console.log('server live');
-    })
-})                            
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+//404
+app.use("*", (req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
+// start server with db
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log("server live");
+  });
+});
